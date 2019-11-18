4cd0a98679a20e5494a575d7dbfcabc78f8b2a7e
Candy Wang <candywang013@gmail.com>
2019-11-18 03:15:54 -0800

    Implement App component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>hello from the react side</div>
+    )
+  }
+}
+
+export default App;
