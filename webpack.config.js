8be4cadaca5ccdef61deba0e2e55cae2cb94426a
Candy Wang <candywang013@gmail.com>
2019-11-18 03:06:02 -0800

    Implement webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'production',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
