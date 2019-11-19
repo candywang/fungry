c41e2de4701d309d23b30b9c4bc284fa7ec4cae4
Candy Wang <candywang013@gmail.com>
2019-11-18 21:37:46 -0800

    Implement App class with ItemRatingForm and ListItem component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import ItemRatingForm from './ItemRatingForm.jsx';
+import ListItem from './ListItem.jsx';
+import myList from '../../../sampleData.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      randomRestaurant: [],
+      userList: myList
     }
   }
 
   render() {
     return (
-      <div>hello from the react side</div>
+        <div>
+          <h1>Fungry.</h1>
+          <h3>Fungry and can't decide where to eat? Press the button below to find a restaurant!</h3>
+          <h5>Rate the item you just tried - whether it was the best you ever had or the worst you've ever had.</h5>
+          <ItemRatingForm />
+          <h5>Items you've tried:</h5>
+          <ListItem userList={this.state.userList} />
+        </div>
     )
   }
 }
