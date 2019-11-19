3b38700b6c52c584815c39e64c0356b2c47ac925
Candy Wang <candywang013@gmail.com>
2019-11-19 02:45:32 -0800

    Implement components with new data set

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -5,13 +5,15 @@ const ListItem = (props) => {
   const userList = props.userList;
   const listItem = userList.map((item) =>
     <ListItemEntry
-      key={item.id}
-      establishment={item.establishment}
-      address={item.address}
+      key={item._id}
+      placeName={item.place_name}
+      category={item.category}
+      date={item.date}
       comment={item.comment}
       rating={item.rating}
-      />
-    );
+      href={`https://www.google.com/maps/search/?api=1&query=Google&query_place_id=${item.place_id}`}
+    />
+  );
 
   return (
     <div><ul>{listItem}</ul></div>
--- client/src/components/ListItemEntry.jsx
+++ client/src/components/ListItemEntry.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ListItemEntry = (props) => {
   return (
-    <div><li>{props.establishment} {props.address} {props.comment} {props.rating}</li></div>
+    <div>
+      <a href={props.href}>{props.placeName}</a>
+      {props.comment} {props.rating}
+    </div>
   )
 }
 
