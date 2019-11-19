f91fb409c195793abaa182d6e3aa2ae525060ce9
Candy Wang <candywang013@gmail.com>
2019-11-19 02:44:53 -0800

    Implement get and post routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('../database/index.js');
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/reviews', (req, res) => {
+  db.get(req, res);
+});
 
-
+app.post('/reviews', (req, res) => {
+  console.log(req.body)
+  db.save(req.body, res);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
