5b63ecbb9180198b10b348c07054780fae99c726
Candy Wang <candywang013@gmail.com>
2019-11-18 21:34:47 -0800

    Implement autocomplete feature using Google Maps Places api

---
+++ client/src/components/GooglePlaces.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+ 
+class GooglePlaces extends React.Component {
+  constructor(props) {
+    super(props);
+		this.state = {
+      address: '',
+      result: []
+    };
+		
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSelect = this.handleSelect.bind(this);
+	}
+ 
+  handleChange(address) {
+    this.setState({ address: address });
+  };
+ 
+  handleSelect(address) {
+    this.setState({address: address});
+    geocodeByAddress(address)
+      .then(results => {
+        this.setState({result: results})
+        this.props.getAddress(this.state);
+      })
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
+ 
+  render() {
+    return (
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Blue Bottle San Francisco',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
+}
+
+export default GooglePlaces;
