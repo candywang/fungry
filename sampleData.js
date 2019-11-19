33c69d9a73d4580b0a5f2a4da00fa2067050064a
Candy Wang <candywang013@gmail.com>
2019-11-19 02:43:50 -0800

    Implement sample data

---
+++ sampleData.js
@@ -0,0 +1,250 @@
+const myList = [
+  {
+      "category": [
+          "cafe",
+          "establishment",
+          "food",
+          "point_of_interest",
+          "store"
+      ],
+      "address": [
+          {
+              "long_name": "168",
+              "short_name": "168",
+              "types": [
+                  "street_number"
+              ]
+          },
+          {
+              "long_name": "2nd Street",
+              "short_name": "2nd St",
+              "types": [
+                  "route"
+              ]
+          },
+          {
+              "long_name": "Yerba Buena",
+              "short_name": "Yerba Buena",
+              "types": [
+                  "neighborhood",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "San Francisco",
+              "short_name": "SF",
+              "types": [
+                  "locality",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "San Francisco County",
+              "short_name": "San Francisco County",
+              "types": [
+                  "administrative_area_level_2",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "California",
+              "short_name": "CA",
+              "types": [
+                  "administrative_area_level_1",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "United States",
+              "short_name": "US",
+              "types": [
+                  "country",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "94105",
+              "short_name": "94105",
+              "types": [
+                  "postal_code"
+              ]
+          }
+      ],
+      "_id": "5dd3c6623b2eef8cc693df4e",
+      "place_id": "ChIJKyGeO8uBhYARGBl0pZptD8c",
+      "place_name": "Blue Bottle Coffee",
+      "item": "iced latte",
+      "rating": 10,
+      "comment": "So delicious! I'm not a huge fan of coffee, but this is one of the best lattes I've ever had! I love that they use organic milk and even carry oat milk.",
+      "date": "2019-11-19T10:39:30.600Z",
+      "__v": 0
+  },
+  {
+      "category": [
+          "bar",
+          "establishment",
+          "food",
+          "point_of_interest",
+          "restaurant"
+      ],
+      "address": [
+          {
+              "long_name": "800",
+              "short_name": "800",
+              "types": [
+                  "street_number"
+              ]
+          },
+          {
+              "long_name": "North Point Street",
+              "short_name": "North Point St",
+              "types": [
+                  "route"
+              ]
+          },
+          {
+              "long_name": "Fisherman's Wharf",
+              "short_name": "Fisherman's Wharf",
+              "types": [
+                  "neighborhood",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "San Francisco",
+              "short_name": "SF",
+              "types": [
+                  "locality",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "San Francisco County",
+              "short_name": "San Francisco County",
+              "types": [
+                  "administrative_area_level_2",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "California",
+              "short_name": "CA",
+              "types": [
+                  "administrative_area_level_1",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "United States",
+              "short_name": "US",
+              "types": [
+                  "country",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "94109",
+              "short_name": "94109",
+              "types": [
+                  "postal_code"
+              ]
+          },
+          {
+              "long_name": "1228",
+              "short_name": "1228",
+              "types": [
+                  "postal_code_suffix"
+              ]
+          }
+      ],
+      "_id": "5dd3c6c9c957e78d321e3db4",
+      "place_id": "ChIJq342JeGAhYARwdtH9tlX7gg",
+      "place_name": "Gary Danko",
+      "item": "5 course meal",
+      "rating": 7,
+      "comment": "I had an amazing time and each dish was amazing, but not worth $500 pp.",
+      "date": "2019-11-19T10:41:13.560Z",
+      "__v": 0
+  },
+  {
+      "category": [
+          "establishment",
+          "food",
+          "point_of_interest",
+          "restaurant"
+      ],
+      "address": [
+          {
+              "long_name": "115",
+              "short_name": "115",
+              "types": [
+                  "street_number"
+              ]
+          },
+          {
+              "long_name": "New Montgomery Street",
+              "short_name": "New Montgomery St",
+              "types": [
+                  "route"
+              ]
+          },
+          {
+              "long_name": "Yerba Buena",
+              "short_name": "Yerba Buena",
+              "types": [
+                  "neighborhood",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "San Francisco",
+              "short_name": "SF",
+              "types": [
+                  "locality",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "San Francisco County",
+              "short_name": "San Francisco County",
+              "types": [
+                  "administrative_area_level_2",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "California",
+              "short_name": "CA",
+              "types": [
+                  "administrative_area_level_1",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "United States",
+              "short_name": "US",
+              "types": [
+                  "country",
+                  "political"
+              ]
+          },
+          {
+              "long_name": "94105",
+              "short_name": "94105",
+              "types": [
+                  "postal_code"
+              ]
+          }
+      ],
+      "_id": "5dd3c721c957e78d321e3db5",
+      "place_id": "ChIJEwcBSn2AhYARc5-ld-6einU",
+      "place_name": "The Bird",
+      "item": "spicy chicken sandwich",
+      "rating": 6,
+      "comment": "Chicken is really juicy and with the right amount of spice. The bun is not good and tastes stale. Every time I eat here my stomach hurts, yet I still come back since there is no chick-fil-a around here.",
+      "date": "2019-11-19T10:42:41.433Z",
+      "__v": 0
+  }
+];
+
+module.exports = myList;
