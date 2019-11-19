e9333917b8b31c9e08574e2d497993115d3175ea
Candy Wang <candywang013@gmail.com>
2019-11-18 21:39:01 -0800

    Require body-parser

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
