72a6f4bb24e2278b212a12dfd8b31ee07dadc3d3
Candy Wang <candywang013@gmail.com>
2019-11-19 02:44:31 -0800

    Implement get and post requests

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
+import Axios from 'axios';
 import ItemRatingForm from './ItemRatingForm.jsx';
 import ListItem from './ListItem.jsx';
-import myList from '../../../sampleData.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       randomRestaurant: [],
-      userList: myList
+      userList: []
     }
 
     this.getForm = this.getForm.bind(this);
+    this.getData = this.getData.bind(this);
   }
 
-  getForm(rating) {
-    console.log(rating, 'rating from app');
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    Axios.get('/reviews')
+      .then(result => {
+        this.setState({userList: result.data});
+      });
+  }
+
+  getForm(review) {
+    console.log(review, 'rating from app');
+    let newReview = review;
+
+    Axios.post('/reviews', newReview)
+      .then(result => console.log(newReview, ' posted! Response: ', response))
+      .catch(err);
   }
 
   render() {
+    console.log(this.state.userList)
     return (
         <div>
           <h1>Fungry.</h1>
           <h3>Fungry and can't decide where to eat? Press the button below to find a restaurant!</h3>
-          <h5>Rate the item you just tried - whether it was the best you ever had or the worst you've ever had.</h5>
+          <h5>Rate the item you just tried - whether it was the best or worst you've ever had.</h5>
           <ItemRatingForm getForm={this.getForm} />
           <h5>Items you've tried:</h5>
           <ListItem userList={this.state.userList} />
